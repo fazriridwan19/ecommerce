682098b204200fa95d90b05e58fc7d28d57bbc0c
Fazri Ridwan <fazriridwan19@gmail.com>
2022-08-08 18:20:22 +0700

    completing homepage

---
+++ src/pages/ProductList.jsx
@@ -0,0 +1,75 @@
+import styled from "styled-components";
+import Announcement from "../components/Announcement";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Newsletter from "../components/Newsletter";
+import Products from "../components/Products";
+
+const Container = styled.div``;
+const Title = styled.h1`
+  margin: 20px;
+`;
+const FilterContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+const Filter = styled.div`
+  margin: 20px;
+`;
+const FilterText = styled.span`
+  font-size: 20px;
+  font-weight: 600;
+  margin-right: 20px;
+`;
+const Select = styled.select`
+  padding: 10px;
+  margin-right: 20px;
+`;
+const Option = styled.option``;
+
+const ProductList = () => {
+  return (
+    <Container>
+      <Announcement />
+      <Navbar />
+      <Title>Dresses</Title>
+      <FilterContainer>
+        <Filter>
+          <FilterText>Filter Products :</FilterText>
+          <Select>
+            <Option disabled selected>
+              Categories
+            </Option>
+            <Option>Smartphone</Option>
+            <Option>Accesories</Option>
+            <Option>Computer</Option>
+            <Option>Gaming</Option>
+          </Select>
+          <Select>
+            <Option disabled selected>
+              Brand
+            </Option>
+            <Option>Apple</Option>
+            <Option>Samsung</Option>
+            <Option>Xiaomi</Option>
+            <Option>Logitech</Option>
+            <Option>Intel</Option>
+          </Select>
+        </Filter>
+        <Filter>
+          <FilterText>Sort Products :</FilterText>
+          <Select>
+            <Option selected>Newest</Option>
+            <Option>Price (asc)</Option>
+            <Option>Price (desc)</Option>
+          </Select>
+        </Filter>
+      </FilterContainer>
+      <Products />
+      <Newsletter />
+      <Footer />
+    </Container>
+  );
+};
+
+export default ProductList;
